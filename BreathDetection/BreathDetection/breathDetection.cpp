3efd66001f0ff6835901592259a8200d0716e0e6
Vit <alfredfid@gmail.com>
2015-03-16 17:31:29 +0300

    some comments

--- BreathDetection/BreathDetection/breathDetection.cpp
+++ BreathDetection/BreathDetection/breathDetection.cpp
@@ -13,17 +13,17 @@
 void breathDetection::_prepareOpenCL() {
 	/* prepare opencl device to work */
 
+	// get info about platform 
 	cl::Platform::get(&_platforms);
-
 	cl::Platform defaultPlatform = _platforms[0];
-	std::cout << "Using platform: " << defaultPlatform.getInfo<CL_PLATFORM_NAME>() << "\n";
+	std::cout << "Using platform: " << defaultPlatform.getInfo<CL_PLATFORM_NAME>();
 
+	// get info about devices 
 	defaultPlatform.getDevices(CL_DEVICE_TYPE_ALL, &_devices);
-
-	// use GPU
 	cl::Device default_device = _devices[0];
 	std::cout << "\nDevice: " << default_device.getInfo<CL_DEVICE_NAME>();
 
+	// organize context 
 	_context = cl::Context(default_device);
 	cl::Program::Sources sources;
 
@@ -33,10 +33,9 @@ void breathDetection::_prepareOpenCL() {
 	kernelCL.close();
 
 	sources.push_back({ kernel_code.c_str(), kernel_code.length() });
-
 	_program = cl::Program(_context, sources);
 
 	// use compiler logs 
-	if (_program.build(_devices, "") == CL_SUCCESS) std::cout << "\nSuccessfully built! " << _program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device) << "\n";
-											   else std::cout << "\nErrors!\n" << _program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(_devices[0]) << "\n";
+	if (_program.build(_devices, "") == CL_SUCCESS) std::cout << "\nSuccessfully built! " << _program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device) << "\n---------------------------------------------";
+											   else std::cout << "\nErrors!\n----------------------------------------" << _program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(_devices[0]) << "\n";
 }
