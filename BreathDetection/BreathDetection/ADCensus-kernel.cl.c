cdd476b4bd714ea928107d7beeb3446c3ff9d7be
Vit <alfredfid@gmail.com>
2015-03-18 14:33:11 +0300

    add ad metric for test

--- BreathDetection/BreathDetection/ADCensus-kernel.cl.c
+++ BreathDetection/BreathDetection/ADCensus-kernel.cl.c
@@ -2,6 +2,7 @@
 
 #define WIDTH 640
 #define HEIGHT 480
+#define SQUARE (WIDTH * HEIGHT)
 #define DISP_MAX 100
 #define DISP_MIN 0
 
@@ -23,4 +24,9 @@ __kernel void kComputeCosts(__global uchar* L,
 						 R[3 * (xyz.x + xyz.x * WIDTH - xyz.z - _DISPARITY_LEVEL_MIN) + 1],
 					     R[3 * (xyz.x + xyz.x * WIDTH - xyz.z - _DISPARITY_LEVEL_MIN) + 2],
 					     0 };
+
+	costs[xyz.x + xyz.y * WIDTH + xyz.z * SQUARE] = (float)(abs_diff(pixL.x, pixR.x) +
+															abs_diff(pixL.y, pixR.y) +
+															abs_diff(pixL.z, pixR.z)
+															) / 3;
 }
